bc16cbc74cfbc29885921e61cf2a3949f2296522
Daan Eeckloo <daan.eeckloo@gmail.com>
2026-09-12 14:43:56 +0200

    added tanh activation

--- nnlib_py/network.py
+++ nnlib_py/network.py
@@ -27,6 +27,17 @@ class Activations:
         s = 1 / (1 + np.exp(-np.clip(x, -500, 500)))
         return s * (1.0 - s)
 
+    @staticmethod
+    def tanh(x):
+        clipped_x = np.clip(x, -500, 500)
+        return np.tanh(clipped_x)
+
+    @staticmethod
+    def tanh_grad(x):
+        clipped_x = np.clip(x, -500, 500)
+        t = np.tanh(clipped_x)
+        return 1.0 - t ** 2
+
     gradient_map = {
         relu.__func__: relu_grad.__func__,
         linear.__func__: linear_grad.__func__,
